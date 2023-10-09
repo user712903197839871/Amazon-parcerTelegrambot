e0c3c11390b99efefbc246026441597ddcb04e61
user712903197839871 <125180474+user712903197839871@users.noreply.github.com>
2023-10-09 15:36:16 +0300

    Update script.sh

--- main_bot/script.sh
+++ main_bot/script.sh
@@ -1,4 +1,9 @@
-pip3 install -r "requirements.txt"
+pip3 install six==1.15.0
+pip3 install certifi==2020.11.8
+pip3 install urllib3>=1.26.5
+pip3 install python-telegram-bot
+pip3 install python_dateutil==2.8.2
+pip3 install simplejson==3.17.5
+
 python3 setup.py build
 python3 setup.py install
-
